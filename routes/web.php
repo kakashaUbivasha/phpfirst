76d0b94c4a671835650dc9af37449ef6e73d939d
kakashaUbivasha <makspala777@gmail.com>
2025-02-10 21:10:53 +0500

    seven

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Post\IndexController;
@@ -21,10 +22,8 @@ use App\Http\Controllers\Post\DestroyController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::group(['prefix' => 'posts'], function () {
+
+Route::group(['prefix' => 'posts', 'middleware'=>'post'], function () {
     Route::get('/', IndexController::class)->name('post.index');
     Route::get('/create', CreateController::class)->name('post.create');
 
@@ -34,13 +33,7 @@ Route::group(['prefix' => 'posts'], function () {
     Route::patch('/{post}', UpdateController::class)->name('post.update');
     Route::delete('/{post}', DestroyController::class)->name('post.destroy');
 });
-
-
-
-//Route::get('posts/update', [PostController::class, 'update']);
-//Route::get('posts/delete', [PostController::class, 'delete']);
-//Route::get('posts/first_or_create', [PostController::class, 'firstOrCreate']);
-//Route::get('posts/update_or_create', [PostController::class, 'updateOrCreate']);
+//Route::get('main', MainController::class)->name('main.index');
 Route::get('books', [BookController::class, 'index'])->name('book.index');
 Route::get('books/create', [BookController::class, 'create'])->name('book.create');
 Route::post('books', [BookController::class, 'store'])->name('book.store');
@@ -48,3 +41,7 @@ Route::get('books/{book}', [BookController::class, 'show'])->name('book.show');
 Route::get('books/{book}/edit', [BookController::class, 'edit'])->name('book.edit');
 Route::patch('books/{book}', [BookController::class, 'update'])->name('book.update');
 Route::delete('books/{book}', [BookController::class, 'destroy'])->name('book.destroy');
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
